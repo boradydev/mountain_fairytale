50dc78a27560959af145ffb1ea5a53d32b4d8c88
boradydev <alexvlod85@gmail.com>
2026-09-21 19:55:14 +0300

    refactor: Move `sales_rep_dialog.dart` and auto-select newly added sales representative in `ClientDialog`
    - Move `sales_rep_dialog.dart` from `delivery_days` to `common` screens directory and update its import in `client_dialog.dart`.
    - Update `ClientDialog` to automatically select the newly added sales representative (`_selectedSalesRepId`) upon successfully creating one.

--- lib/presentation/screens/common/client_dialog.dart
+++ lib/presentation/screens/common/client_dialog.dart
@@ -3,7 +3,7 @@ import 'package:mountain_fairytale/infra/app_notify.dart';
 import 'package:mountain_fairytale/infra/repos/clients/models/client_model.dart';
 import 'package:mountain_fairytale/infra/repos/sales_representatives/models/sales_representative_model.dart';
 import 'package:mountain_fairytale/presentation/providers/clients_provider.dart';
-import 'package:mountain_fairytale/presentation/screens/delivery_days/sales_rep_dialog.dart';
+import 'package:mountain_fairytale/presentation/screens/common/sales_rep_dialog.dart';
 import 'package:mountain_fairytale/presentation/widgets/base_form_dialog_widget.dart';
 import 'package:mountain_fairytale/presentation/widgets/confirm_delete_dialog.dart';
 import 'package:mountain_fairytale/presentation/widgets/duplicate_check_status_widget.dart';
@@ -391,7 +391,12 @@ class _ClientDialogState extends State<ClientDialog> {
                 );
 
                 if (result == true && mounted) {
-                  setState(() {});
+                  // Получаем последнего добавленного представителя из списка провайдера
+                  final lastAddedRep = provider.salesRepresentatives.first;
+
+                  setState(() {
+                    _selectedSalesRepId = lastAddedRep.id;
+                  });
                 }
               },
               onEdit: currentSelection == null
