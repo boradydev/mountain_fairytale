b7e045fc6ff0c74cfef38cb8767789217a7f217c
boradydev <alexvlod85@gmail.com>
2026-09-25 21:19:18 +0300

    refactor: Remove PDF viewer and URL launcher dependencies
    - Remove `pdfrx` and `url_launcher` packages along with their platform-specific dependencies and plugins from `pubspec.yaml`, `pubspec.lock`, and Windows configuration files.
    - Update the minimum Flutter SDK constraint in `pubspec.yaml`.

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,12 +6,10 @@ list(APPEND FLUTTER_PLUGIN_LIST
   desktop_drop
   printing
   screen_retriever_windows
-  url_launcher_windows
   window_manager
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
-  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
